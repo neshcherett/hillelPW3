9649c4f616db9a675aadd8dc1c789d6c76a117a9
Tetiana Neshcheret <neshcherett10@gmail.com>
2021-03-23 20:41:32 +0200

    Added new truck ford

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         TruckEngine truckEngine = new TruckEngine("M1", 6.0);
+        Truck ford = new Truck("Ford", "m1", truckEngine, 85);
         Truck volvo = new Truck("Volvo", "model1", truckEngine, 80);
         volvo.move();
     }
